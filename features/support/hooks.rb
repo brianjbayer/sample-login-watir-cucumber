b353fcf63c7947b3170b419405077e251f2dec57
brianjbayer <brianjbayer@gmail.com>
2017-12-15 18:34:56 -0500

    cleanup

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,27 +1,24 @@
 Before do
-  if @browser.nil?
-    puts '<<< INIT BROWSER >>>'
-    case ENV['SPEC_BROWSER']
+  case ENV['SPEC_BROWSER']
 
-    when 'chrome'
-      @browser = Watir::Browser.new :chrome
+  when 'chrome'
+    @browser = Watir::Browser.new :chrome
 
-    when 'chrome_headless', 'headless_chrome'
-      @browser = Watir::Browser.new :chrome, headless: true
+  when 'chrome_headless', 'headless_chrome'
+    @browser = Watir::Browser.new :chrome, headless: true
 
-    when 'firefox'
-      @browser = Watir::Browser.new :firefox
+  when 'firefox'
+    @browser = Watir::Browser.new :firefox
 
-    when 'safari'
-      @browser = Watir::Browser.new :safari, technology_preview: true
+  when 'safari'
+    @browser = Watir::Browser.new :safari
 
-    else
-      STDERR.puts '>> USING DEFAULT DRIVER <<'
-      @browser = Watir::Browser.new
-    end
-
-    @browser.driver.manage.timeouts.implicit_wait = 15
+  else
+    STDERR.puts '>> USING DEFAULT DRIVER <<'
+    @browser = Watir::Browser.new
   end
+
+  @browser.driver.manage.timeouts.implicit_wait = 15
 end
 
 After do
