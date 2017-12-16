a3874e51cf5d73ab1647a1ca325828d947ebd27e
brianjbayer <brianjbayer@gmail.com>
2017-12-16 16:42:52 -0500

    refactored and rubocopped

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,63 +1,23 @@
-Before do
-  if ENV['SPEC_BROWSER'] 
-    # A browser has been specified, we will assume the caller knows the supported watir browser names
-    # with some special cases of course
-    browser_name = ENV['SPEC_BROWSER'].strip
-    
-    # Let's deal with the special cases
-    # TODO Refactor the headless case to algorithm
-    case browser_name
-      
-    when 'chrome_headless', 'headless_chrome'
-      @browser = Watir::Browser.new :chrome, headless: true
 
-    when 'firefox_headless', 'headless_firefox'
-      @browser = Watir::Browser.new :firefox, headless: true
-      
-    else
-      # Just symbolize it and pass it thru
-      @browser = Watir::Browser.new browser_name.to_sym
-    end
-  else
-    STDERR.puts '>> USING DEFAULT DRIVER <<'
-    @browser = Watir::Browser.new
-
-  end
-  
-  # 
-  #   if browser_name == 'chrome_headless'
-  #     @browser = Watir::Browser.new browser: :chrome, headless: true
-  #   else
-  #     # Just symbolize it and pass it thru
-  #       @browser = Watir::Browser.new browser_name.to_sym
-  #   end
-  # else
-  #   STDERR.puts '>> USING DEFAULT DRIVER <<'
-  #   @browser = Watir::Browser.new 
-  # end
+def headless_browser(headless_browser)
+  # split on underscores or whitespace
+  remove_headless = headless_browser.tr('_', ' ').split
+  remove_headless.delete('headless')
+  remove_headless.join('_')
 end
 
-#   case ENV['SPEC_BROWSER']
-# 
-#   when 'chrome'
-#     @browser = Watir::Browser.new :chrome
-# 
-#   when 'chrome_headless', 'headless_chrome'
-#     @browser = Watir::Browser.new :chrome, headless: true
-# 
-#   when 'firefox'
-#     @browser = Watir::Browser.new :firefox
-# 
-#   when 'safari'
-#     @browser = Watir::Browser.new :safari
-# 
-#   else
-#     STDERR.puts '>> USING DEFAULT DRIVER <<'
-#     @browser = Watir::Browser.new
-#   end
-# 
-#   @browser.driver.manage.timeouts.implicit_wait = 15
-# end
+Before do
+  if ENV['SPEC_BROWSER']
+    browser_name = ENV['SPEC_BROWSER'].downcase.strip
+    @browser = if browser_name.include?('headless')
+                 Watir::Browser.new headless_browser(browser_name).to_sym, headless: true
+               else Watir::Browser.new browser_name.to_sym
+               end
+  else
+    STDERR.puts '>> USING DEFAULT (Watir) DRIVER <<'
+    @browser = Watir::Browser.new
+  end
+end
 
 After do
   @browser.close
