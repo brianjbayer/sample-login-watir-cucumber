ea88d3900d7600b54071c9bd27308633dcfb4afd
brianjbayer <brianjbayer@gmail.com>
2019-08-26 15:26:56 -0400

    rubocopped

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,33 +2,36 @@
 
 require 'webdrivers'
 
-def configure_browser(specified_browser)
-  specified_browser = ENV['SPEC_BROWSER'].downcase.strip
-  browser_options = specified_browser.tr('_', ' ').split
-
-  headless = ! browser_options.delete('headless').nil?
-  container = ! browser_options.delete('container').nil?
-  # Assume whatever is left is the browser type (e.g. chrome)
-  browser_type = browser_options.first.to_sym
-
-  @browser = if container
-    selenium_standalone = 'http://localhost:4444/wd/hub'
-    Watir::Browser.new browser_type, url: selenium_standalone, headless: headless
-  else
-    Watir::Browser.new browser_type, headless: headless
-  end
-end
-
+STANDALONE = 'http://localhost:4444/wd/hub'
 
 Before do
   @browser = if ENV['SPEC_BROWSER']
-    configure_browser ENV['SPEC_BROWSER']
-  else
-    warn '>> USING DEFAULT (Watir) DRIVER <<'
-   Watir::Browser.new
-  end
+               create_browser ENV['SPEC_BROWSER'].downcase.strip
+             else
+               warn '>> USING DEFAULT (Watir) DRIVER <<'
+               Watir::Browser.new
+             end
 end
 
 After do
   @browser.close
+end
+
+def create_browser(specified_browser)
+  browser_options = specified_browser.tr('_', ' ').split
+
+  headless = !browser_options.delete('headless').nil?
+  container = !browser_options.delete('container').nil?
+  # Assume whatever is left is the browser type (e.g. chrome)
+  browser_type = browser_options.first.to_sym
+
+  configure_browser(browser_type, headless, container)
+end
+
+def configure_browser(browser_type, headless, container)
+  if container
+    Watir::Browser.new browser_type, url: STANDALONE, headless: headless
+  else
+    Watir::Browser.new browser_type, headless: headless
+  end
 end
